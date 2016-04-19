0d84bbc22266f3d6a8c5bdb762af1385b00545b0
System Administrator <root@Jamess-MacBook-Pro-3.local>
2016-04-18 22:48:47 -0400

    repeated_random

--- repeated_random.py
+++ repeated_random.py
@@ -1,3 +1,40 @@
-import os # to read in inputfile
-inputfile = os.argv[1]
+#!/usr/bin/env python
+# boilerplate usage / reading in file
+## Usage: ./repeated_random.py path_to_input_file max_iterations
+## returns random sequence for input_file with smallest residue after max_iterations
+import sys # to read in inputfile
+if len(sys.argv) != 3:
+	print "Usage: ./repeated_random.py path_to_input_file max_iterations"
+	sys.exit(0)
+input_file = sys.argv[1]
+max_iterations = sys.argv[2]
+# most memory conservative way to read it in 
+# but shouldn't be an issue because 100 or less
+our_list = []
+with open(input_file) as FileObj:
+    for line in FileObj:
+       our_list.append(int(line)) # cast to int
+# end boilerplate, begin repeated_random
+
+def residue(S, A):
+	sigma = 0
+	for p, a in zip(S, A):
+		sigma += p * a
+	return sigma
+
+from random import choice # choosing between -1 and 1
+best_sequence = [random.choice([-1, 1]) for _ in range(0,len(our_list))]
+best_residue = residue(best_sequence)
+for (i = max_iterations; i > 0; i--):
+	random_assignment = [random.choice([-1, 1]) for _ in range(0,len(our_list))]
+	this_residue = residue(random_assignment)
+	if this_residue < best_residue:
+		best_sequence = random_assignment
+		best_residue = this_residue
+# returns the best sequence
+print best_sequence
+
+
+
+
 
