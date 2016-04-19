d6023f08f63bc1745195ca5e493d560ca48e4d5c
James Dreben <jamesdreben@Jamess-MacBook-Pro-3.local>
2016-04-18 22:51:21 -0400

    slight fix to repeated_random

--- repeated_random.py
+++ repeated_random.py
@@ -7,7 +7,7 @@ if len(sys.argv) != 3:
 	print "Usage: ./repeated_random.py path_to_input_file max_iterations"
 	sys.exit(0)
 input_file = sys.argv[1]
-max_iterations = sys.argv[2]
+max_iterations = int(sys.argv[2])
 # most memory conservative way to read it in 
 # but shouldn't be an issue because 100 or less
 our_list = []
@@ -23,11 +23,11 @@ def residue(S, A):
 	return sigma
 
 from random import choice # choosing between -1 and 1
-best_sequence = [random.choice([-1, 1]) for _ in range(0,len(our_list))]
-best_residue = residue(best_sequence)
-for (i = max_iterations; i > 0; i--):
-	random_assignment = [random.choice([-1, 1]) for _ in range(0,len(our_list))]
-	this_residue = residue(random_assignment)
+best_sequence = [choice([-1, 1]) for _ in range(0,len(our_list))]
+best_residue = residue(best_sequence, our_list)
+for i in range(max_iterations, 0, -1):
+	random_assignment = [choice([-1, 1]) for _ in range(0,len(our_list))]
+	this_residue = residue(random_assignment, our_list)
 	if this_residue < best_residue:
 		best_sequence = random_assignment
 		best_residue = this_residue
