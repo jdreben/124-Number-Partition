ec4f79665bae3b9f7c401a132dd13fe2ddac8e65
jdreben <root@Jamess-MacBook-Pro-3.local>
2016-04-20 22:49:04 -0400

    just updating things

--- 50instances.py
+++ 50instances.py
@@ -5,31 +5,41 @@
 
 from kk import run as kk
 from simulated_annealing import simAnneal as simulated_annealing
+from hill_climbing import run as hill_climbing
 from repeated_random import run as repeated_random
 from random import randint
 
 iterations = 25000
-kk_results, sa_results, rr_results  = [], [], []
-for loop in range(0, 50):
-	print "at loop {}".format(str(loop))
+kk_results, sa_results, rr_results, hc_results  = [], [], [], []
+for loop in range(0, 20):
+	print "At loop {}".format(str(loop))
 	# generate a new one each iteration
 	this_loop_list = [randint(0, 10 ** 12) for _ in range(0, 100)]
-	# print "loop is {}, this_loop_list is {}".format(str(loop), str(this_loop_list))
-	kk_result = kk(this_loop_list[:])
 
+	kk_result = kk(this_loop_list[:])
 	kk_results.append(kk_result)
-	# print "kk_result " + str(kk_result)
-	# print "sending this_loop_list " + str(this_loop_list) + " to anneal"
-	sa_result = simulated_annealing(this_loop_list, iterations)
-	sa_results.append(sa_result)
-	# print "sa_result " + str(sa_result)
+	print "kk_result " + str(kk_result)
 	rr_result = repeated_random(this_loop_list, iterations)
 	rr_results.append(rr_result)
-	# print "rr_result " + str(rr_result)
+	print "rr_result " + str(rr_result)
+	hc_result = hill_climbing(this_loop_list, iterations)
+	hc_results.append(hc_result)
+	print "hc_result " + str(hc_result)
+	sa_result = simulated_annealing(this_loop_list, iterations)
+	sa_results.append(sa_result)
+	print "sa_result " + str(sa_result)
+	
+
 
 import matplotlib.pyplot as plt
+print "(blue) kk_results {}".format(str(kk_results)) 
+print "(red) sa_results {}".format(str(sa_results)) 
+print "(pink) hc_results {}".format(str(hc_results)) 
+print "(green) rr_results {}".format(str(rr_results)) 
+
 
 plt.plot(kk_results, color='blue')
 plt.plot(sa_results, color='red')
 plt.plot(rr_results, color='green')
+plt.plot(hc_results, color='pink')
 plt.show()
--- hill_climbing.py
+++ hill_climbing.py
@@ -4,17 +4,21 @@
 ## returns hill climbing for input_file with smallest residue after max_iterations
 import sys # to read in inputfile
 if len(sys.argv) != 3:
+	'''
 	print "Usage: ./hill_climbing.py path_to_input_file max_iterations"
 	sys.exit(0)
-input_file = sys.argv[1]
-max_iterations = int(sys.argv[2])
-# most memory conservative way to read it in 
-# but shouldn't be an issue because 100 or less
-our_list = []
-with open(input_file) as FileObj:
-    for line in FileObj:
-       our_list.append(int(line)) # cast to int
-# end boilerplate, begin repeated_random
+	'''
+else:
+	input_file = sys.argv[1]
+	max_iterations = int(sys.argv[2])
+	# most memory conservative way to read it in 
+	# but shouldn't be an issue because 100 or less
+	our_list = []
+	with open(input_file) as FileObj:
+	    for line in FileObj:
+	       our_list.append(int(line)) # cast to int
+	# end boilerplate, begin repeated_random
+	print run(our_list, max_iterations)
 
 def residue(S, A):
 	sigma = 0
@@ -22,30 +26,30 @@ def residue(S, A):
 		sigma += p * a
 	return abs(sigma)
 
-from random import choice, random, randint
+def run(our_list, max_iterations):
+	from random import choice, random, randint
 
-best_sequence = [choice([-1, 1]) for _ in range(0,len(our_list))]
-best_residue = residue(best_sequence, our_list)
+	best_sequence = [choice([-1, 1]) for _ in range(0,len(our_list))]
+	best_residue = residue(best_sequence, our_list)
 
-for i in range(max_iterations, 0, -1):
-	this_sequence = best_sequence[:]; index = None
-	# Choose two random indices i and j from [1,n] with i != j
-	if random() < .5: # 50 / 50 chance of 1 or 2 switched
-		# 2 switched
-		index = randint(0,len(our_list)-1)
-		this_sequence[index] = -1 * this_sequence[index]
-	# 1 switched
-	random_move = index 
-	while True:
-		index = randint(0, len(our_list)-1)
-		if random_move != index:
+	for i in range(max_iterations, 0, -1):
+		this_sequence = best_sequence[:]; index = None
+		# Choose two random indices i and j from [1,n] with i != j
+		if random() < .5: # 50 / 50 chance of 1 or 2 switched
+			# 2 switched
+			index = randint(0,len(our_list)-1)
 			this_sequence[index] = -1 * this_sequence[index]
-			break # makes sure the same one isn't changed twice
-	# check if better than the best
-	this_residue = residue(this_sequence, our_list)
-	if this_residue < best_residue:
-		best_sequence = this_sequence
-		best_residue = this_residue
-# returns the best sequence
-print best_sequence 
-print best_residue
+		# 1 switched
+		random_move = index 
+		while True:
+			index = randint(0, len(our_list)-1)
+			if random_move != index:
+				this_sequence[index] = -1 * this_sequence[index]
+				break # makes sure the same one isn't changed twice
+		# check if better than the best
+		this_residue = residue(this_sequence, our_list)
+		if this_residue < best_residue:
+			best_sequence = this_sequence
+			best_residue = this_residue
+	# returns the best sequence
+	return best_residue
